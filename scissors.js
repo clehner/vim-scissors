b0bad8568314d7e5e2ef4b398bbc58c29b5bccf3
Charles Lehner <celehner1@gmail.com>
2013-07-03 01:06:17 -0400

    Initial commit

---
+++ scissors.js
@@ -0,0 +1,101 @@
+var sheets = {};
+
+function cssRuleToText(rule) {
+	return rule.cssText;
+}
+
+function ruleDiffToText(rule) {
+	var declarations = [];
+	for (var prop in rule.style) {
+		declarations.push(prop + ': ' + rule.style[prop] + ';');
+	}
+	return rule.selectorText + ' {' + declarations.join(' ') + ' }';
+}
+
+// wrap and sync a CSSStyleSheet
+function Sheet(sheet, i) {
+	if (!(this instanceof Sheet)) return new Sheet(sheet, i);
+	this.sheet = sheet;
+	if (sheet.ownerNode.nodeName == 'STYLE') {
+		// todo: generate name based on page filename
+		this.name = 'style' + i + '.css';
+	} else {
+		this.name = sheet.href.substr(sheet.href.lastIndexOf('/') + 1);
+	}
+	this.cssRules = [].slice.call(sheet.rules);
+}
+
+Sheet.prototype.openOnServer = function() {
+	conn.send(JSON.stringify({
+		type: "openSheet",
+		sheetName: this.name,
+		sheetRules: this.cssRules.map(cssRuleToText)
+	}));
+};
+
+Sheet.prototype.applyDiff = function(rulesDiff) {
+	var rules = this.cssRules;
+	var offset = 0;
+	var sheet = this.sheet;
+	rulesDiff.forEach(function (ruleDiff, i) {
+		var index = ruleDiff.index + offset;
+		var rule = rules[index];
+		if (ruleDiff.remove) {
+			sheet.deleteRule(index);
+			rules.splice(index, 1);
+			offset--;
+			return;
+		}
+		if (!rule) {
+			sheet.insertRule(ruleDiffToText(ruleDiff), index);
+			rule = sheet.cssRules[index];
+			rules.splice(index, 0, rule);
+			offset++;
+		}
+		if (ruleDiff.selectorText) {
+			rule.selectorText = ruleDiff.selectorText;
+		}
+		for (var prop in ruleDiff.style) {
+			rule.style[prop] = ruleDiff.style[prop] || '';
+		}
+	});
+};
+
+function captureStylesheets() {
+	[].slice.call(document.styleSheets).map(Sheet).forEach(function (sheet) {
+		sheets[sheet.name] = sheet;
+		sheet.openOnServer();
+	});
+}
+
+var scripts = document.getElementsByTagName('script');
+var scriptSrc = scripts[scripts.length-1].src;
+var url = 'ws' + scriptSrc.match(/s?:s?.*\//);
+var conn = new WebSocket(url);
+
+conn.onerror = function(err) {
+	alert(err);
+};
+
+conn.onopen = captureStylesheets;
+
+conn.onmessage = function(msg) {
+	var event;
+	try {
+		event = JSON.parse(msg.data);
+	} catch(e) {
+		return;
+	}
+
+	if (event.type == 'rulesDiff') {
+		var sheet = sheets[event.sheetName];
+		if (!sheet) {
+			console.error('Unknown sheet', event.sheetName);
+			return;
+		}
+		sheet.applyDiff(event.rulesDiff);
+	}
+};
+
+conn.onclose = function() {
+};
---
+++ server.js
@@ -0,0 +1,157 @@
+/*jshint indent:2 */
+var log    = console.log.bind(log);
+var WebSS  = require('ws').Server;
+var http   = require('http');
+var nb     = require('vim-netbeans');
+var fs     = require('fs');
+var Sheet  = require('./lib/sheet');
+var debounce = require('./lib/debounce');
+
+var nbServer = new nb.VimServer({'debug': 0});
+var sheets = {};
+
+var staticFiles = {
+  '/': 'app.html',
+  '/style.css': 0,
+  '/scissors.js': 0
+};
+
+var textMimes = {
+  js: 'javascript'
+};
+
+var httpServer = http.createServer(function(req, res) {
+  var url = req.url;
+  if (url in staticFiles) {
+    var fileName = staticFiles[url] || ('.' + url);
+    var extension = fileName.substr(fileName.lastIndexOf('.')+1);
+    var format = textMimes[extension] || extension;
+    res.setHeader('Content-Type', 'text/' + format);
+    fs.createReadStream(fileName).pipe(res);
+  } else {
+    res.end();
+  }
+});
+
+nbServer.on('clientAuthed', openSheetsInVimClient);
+nbServer.listen(3221);
+nbServer.handleHTTP(httpServer);
+
+var wss = new WebSS({server: httpServer});
+var sockets = [];
+
+function wsBroadcast(event, cb) {
+  var msg = JSON.stringify(event);
+  sockets.forEach(function(socket) {
+    socket.send(msg, cb);
+  });
+}
+
+function handleError(err) {
+  if (err) console.error(err);
+}
+
+function setBufferText(buffer, text, cb) {
+  buffer.getLength(function (length) {
+    buffer.remove(0, length, function (err) {
+      if (err) return cb(err);
+      buffer.insert(0, text, cb);
+    });
+  });
+}
+
+function openSheetInVimClient(sheet, client) {
+  var buffer, bufferText = '';
+
+  var handleChanges = debounce(function() {
+    var newSheet;
+    try {
+      newSheet = new Sheet(sheet.name, bufferText);
+    } catch(e) {
+      // CSS Error
+      buffer.showBalloon(e.toString().replace(/['"]/g, ''));
+      return;
+    }
+    var diff = sheet.getRulesDiff(newSheet);
+    //console.log('diff', diff);
+    sheet.cssRules = newSheet.cssRules;
+
+    wsBroadcast({
+      type: 'rulesDiff',
+      sheetName: sheet.name,
+      rulesDiff: diff
+    }, handleError);
+  }, 50);
+
+  client.editFile(sheet.name, function(buf) {
+    buffer = buf;
+    var css = sheet.getCSSText();
+    setBufferText(buffer, css, function(err) {
+      if (err) console.error('Error setting text', err);
+      bufferText = css;
+    });
+
+    buffer.on("insert", function(offset, text) {
+      bufferText = bufferText.substr(0, offset) + text + bufferText.substr(offset);
+      handleChanges();
+    });
+    buffer.on("remove", function(offset, length) {
+      bufferText = bufferText.substr(0, offset) + bufferText.substr(offset + length);
+      handleChanges();
+    });
+    buffer.on("fileOpened", function() {
+      buffer.getText(function(text) {
+        bufferText = text;
+        handleChanges();
+      });
+    });
+  });
+}
+
+function openSheetInVimClients(sheet) {
+  nbServer.clients.forEach(function(client) {
+    openSheetInVimClient(sheet, client);
+  });
+}
+
+function openSheetsInVimClient(client) {
+  for (var name in sheets) {
+    var sheet = sheets[name];
+    openSheetInVimClient(sheet, client);
+  }
+}
+
+wss.on('connection', function(ws) {
+  log("> CONNECTION");
+  sockets.push(ws);
+
+  ws.on('close', function() {
+    log("> CLIENT DISCONNECT");
+    sockets.splice(sockets.indexOf(ws), 1);
+  });
+
+  ws.on('message', function(msg) {
+    try {
+      var event = JSON.parse(msg);
+    } catch(e) {
+      return;
+    }
+    if (event.type == 'openSheet') {
+      var name = event.sheetName;
+      var rules = event.sheetRules;
+      var theirSheet = new Sheet(name, rules);
+      var ourSheet = sheets[name];
+      if (ourSheet) {
+        var rulesDiff = theirSheet.getRulesDiff(ourSheet);
+        ws.send(JSON.stringify({
+          type: 'rulesDiff',
+          sheetName: name,
+          rulesDiff: rulesDiff
+        }));
+      } else {
+        sheets[name] = theirSheet;
+        openSheetInVimClients(theirSheet);
+      }
+    }
+  });
+});
